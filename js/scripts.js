673bcc1ffd078124a170b5361fcca99d5d64d2db
Brian Martin <90224019+martinbd1@users.noreply.github.com>
2022-01-30 16:33:47 -0500

    Create an array of 3 pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-alert('Hello world');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [{
+    name: "Bulbasaur",
+    height: 0.7,
+    type: ["grass", "poison"]
+},
+{
+    name: "Charmander",
+    height: 0.6,
+    type: "fire"
+},
+{
+    name: "Squirtle",
+    height: 0.5,
+    type: "water"
+},
+];
