36406ca2d76fe7dd92fc70f7a156414f72cd2f5f
Brian Martin <90224019+martinbd1@users.noreply.github.com>
2022-02-01 17:21:16 -0500

    beautify the code spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 let pokemonList = [{
-    name: "Bulbasaur",
-    height: 0.7,
-    type: ["grass", "poison"]
-},
-{
-    name: "Charmander",
-    height: 0.6,
-    type: ["fire"]
-},
-{
-    name: "Squirtle",
-    height: 0.5,
-    type: ["water"]
-},
+        name: "Bulbasaur",
+        height: 0.7,
+        type: ["grass", "poison"]
+    },
+    {
+        name: "Charmander",
+        height: 0.6,
+        type: ["fire"]
+    },
+    {
+        name: "Squirtle",
+        height: 0.5,
+        type: ["water"]
+    },
 ];
