e10fe11a44a1cd3da9e7d11216ca7b288d885b78
Brian Martin <90224019+martinbd1@users.noreply.github.com>
2022-02-03 19:58:19 -0500

    updated per task 1.5 part 2, part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,43 @@
 // List of pokemon, thier 0name 1height and 2type, some pokemon have more than one type
 
-let pokemonList = [{
-        name: "Bulbasaur",
-        height: 0.7,
-        type: ["grass", "poison"]
-    },
-    {
-        name: "Ivysaur",
-        height: 1,
-        type: ["grass", "poison"]
-    },
-    {
-        name: "Venusaur",
-        height: 2,
-        type: ["grass", "poison"]
-    },
-    {
-        name: "Charmander",
-        height: 0.6,
-        type: ["fire"]
-    },
-    {
-        name: "Squirtle",
-        height: 0.5,
-        type: ["water"]
-    },
-    {
-        name: "Caterpie",
-        height: 0.3,
-        type: ["bug"]
-    },
-    {
-        name: "Weedle",
-        height: 0.3,
-        type: ["bug", "poison"]
-    },
-];
-
-console.log(pokemonList);
-
+let pokemonRepository = (function () {
+    //Now wrappped in an IIED
+    let pokemonList = [{
+            name: "Bulbasaur",
+            height: 0.7,
+            type: ["grass", "poison"]
+        },
+        {
+            name: "Ivysaur",
+            height: 1,
+            type: ["grass", "poison"]
+        },
+        {
+            name: "Venusaur",
+            height: 2,
+            type: ["grass", "poison"]
+        },
+        {
+            name: "Charmander",
+            height: 0.6,
+            type: ["fire"]
+        },
+        {
+            name: "Squirtle",
+            height: 0.5,
+            type: ["water"]
+        },
+        {
+            name: "Caterpie",
+            height: 0.3,
+            type: ["bug"]
+        },
+        {
+            name: "Weedle",
+            height: 0.3,
+            type: ["bug", "poison"]
+        },
+    ];
 
 // a forEach loop that list the pokemons name and (height) and notes bigs ones
 
@@ -50,6 +49,38 @@ console.log(pokemonList);
 //     }
 // })
 
+// function pokemonLoop(pokemon) {
+//     if (pokemon.height > 1) {
+//         document.write("<p>" + pokemon.name + " (height:" + pokemon.height + ") - Wow, that's big!</p>");
+//     } else {
+//         document.write("<p>" + pokemon.name + " (height:" + pokemon.height + ")</p>");
+//     }
+// }
+// pokemonList.forEach(pokemonLoop);
+
+    return {
+        add: function (pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function () {
+            return pokemonList;
+        }
+    };
+
+})();
+
+console.log(pokemonRepository.getAll());
+
+//Adds a new pokemon and attributes to the Array
+pokemonRepository.add({
+    name: "Pikachu",
+    height: 0.4,
+    type: ["electric"]
+});
+
+console.log(pokemonRepository.getAll());
+
+// a forEach loop that list the pokemons name and (height) and notes bigs ones
 function pokemonLoop(pokemon) {
 
     if (pokemon.height > 1) {
@@ -58,4 +89,4 @@ function pokemonLoop(pokemon) {
         document.write("<p>" + pokemon.name + " (height:" + pokemon.height + ")</p>");
     }
 }
-pokemonList.forEach(pokemonLoop);
+pokemonRepository.getAll().forEach(pokemonLoop);
